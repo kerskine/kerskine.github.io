66a7f0facae0b689b8618c62dd00f37c7b4b78a1
Keith Erskine <keith.erskine@gmail.com>
2020-12-15 17:05:38 -0500

    Dec 15 update

--- covid.R
+++ covid.R
@@ -4,7 +4,7 @@ library(readxl)
 
 # Download csv files from Mass Dept of Public Health
 
-download.file("https://www.mass.gov/doc/covid-19-raw-data-december-13-2020/download", "./data/data.zip")
+download.file("https://www.mass.gov/doc/covid-19-raw-data-december-15-2020/download", "./data/data.zip")
 unzip("./data/data.zip", exdir = "./data")
 
 # Read and clean cases
